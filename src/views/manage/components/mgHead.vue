9b13045a3b2562acff5d065ada4328415e38e727
bingxu <984386667@qq.com>
2019-08-22 21:50:05 +0800

    fix bug and add menu for user

--- src/views/manage/components/mgHead.vue
+++ src/views/manage/components/mgHead.vue
@@ -1,7 +1,22 @@
 <template>
   <el-header class="header" style="height: 80px">
-    <div class="title"><i class="el-icon-s-home"></i> xx公司仓库管理系统</div>
-    <div class="user">当前用户：史亚军</div>
+    <div class="cont">
+      <div class="title"><i class="el-icon-s-home"></i> xx公司仓库管理系统</div>
+      <div class="user">
+        <i class="el-icon-user-solid"></i>
+        <el-dropdown>
+          <span class="el-dropdown-link" style="color: #fff;">
+             史亚军 <i class="el-icon-caret-bottom"></i>
+          </span>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item icon="el-icon-message-solid">我的消息</el-dropdown-item>
+            <el-dropdown-item icon="el-icon-alarm-clock">申请进度</el-dropdown-item>
+            <el-dropdown-item icon="el-icon-key">修改密码</el-dropdown-item>
+            <el-dropdown-item icon="el-icon-switch-button">注销登录</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </div>
+    </div>
   </el-header>
 </template>
 
@@ -25,12 +40,20 @@ export default {
   box-sizing: border-box;
   height: 80px;
   width: 100vw;
-  .title {
-    line-height: 80px;
+  color: #fff;
+  font-weight: lighter;
+  line-height: 80px;
+  .cont{
+    .title {
     margin: 0;
     font-size: 30px;
-    color: #fff;
-    font-weight: lighter;
+    width: 50%;
+    float: left;
+  }
+    .user {
+      float: right;
+      margin-top: 7px;
+    }
   }
 }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -3,16 +3,16 @@
 module.exports = {
   // assetsDir: "static",
   devServer: {
-    open: true,
-    host: '0.0.0.0',
-    port: 8000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        ws: true,
-        changeOrigin: true
-      }
-    }
+    open: true
+    // host: '0.0.0.0',
+    // port: 8000
+    // proxy: {
+    //   '/api': {
+    //     target: 'http://localhost:8000',
+    //     ws: true,
+    //     changeOrigin: true
+    //   }
+    // }
   }
   // configureWebpack: {
   //   // 增加一个plugins
